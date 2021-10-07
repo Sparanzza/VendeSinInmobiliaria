90916853d7e52ba90a4353d2ffc25e3172b720b3
Aurelio Muñoz <aurelio@sparanzza.com>
2021-10-08 00:53:24 +0200

    Added Category and basic features information on cardview_post.xml

--- app/src/main/java/com/ilerna/vendesininmobiliarias/adapters/PostsAdapter.java
+++ app/src/main/java/com/ilerna/vendesininmobiliarias/adapters/PostsAdapter.java
@@ -33,16 +33,33 @@ public class PostsAdapter extends FirestoreRecyclerAdapter<Post, PostsAdapter.Vi
     @Override
     protected void onBindViewHolder(@NonNull ViewHolder holder, int position, @NonNull Post model) {
         holder.titlePostTextView.setText(model.getTitle());
-        holder.pricePostTextView.setText( NumberFormat.getNumberInstance(Locale.US).format(Long.parseLong(model.getPrice())) + " $");
+        holder.pricePostTextView.setText(NumberFormat.getNumberInstance(Locale.US).format(Long.parseLong(model.getPrice())) + " $");
+        holder.categoryPostTextView.setText(model.getCategory());
+        holder.locationPostTextView.setText("Location Test");
 
-        if (model.getImage0() != null && !model.getImage0().isEmpty()) setMainPhotoPostImageView(model.getImage0());
-        else if (model.getImage1() != null && !model.getImage1().isEmpty()) setMainPhotoPostImageView(model.getImage1());
-        else if (model.getImage2() != null && !model.getImage2().isEmpty()) setMainPhotoPostImageView(model.getImage2());
-        else if (model.getImage3() != null && !model.getImage3().isEmpty()) setMainPhotoPostImageView(model.getImage3());
-        else if (model.getImage4() != null && !model.getImage4().isEmpty()) setMainPhotoPostImageView(model.getImage4());
-        else if (model.getImage5() != null && !model.getImage5().isEmpty()) setMainPhotoPostImageView(model.getImage5());
-        else if (model.getImage6() != null && !model.getImage6().isEmpty()) setMainPhotoPostImageView(model.getImage6());
-        else if (model.getImage7() != null && !model.getImage7().isEmpty()) setMainPhotoPostImageView(model.getImage7());
+        holder.bedroomPostTextView.setText(model.getBedroom().isEmpty() ? "-" : model.getBedroom());
+        holder.bathroomPostTextView.setText(model.getBathroom().isEmpty() ? "-" : model.getBathroom());
+        holder.sqmPostTextView.setText(model.getSqm().isEmpty() ? "-" : model.getSqm());
+        holder.floorPostTextView.setText(model.getFloor().isEmpty() ? "-" : model.getFloor());
+        holder.antiquityPostTextView.setText(model.getAntiquity().isEmpty() ? "-" : model.getAntiquity());
+
+
+        if (model.getImage0() != null && !model.getImage0().isEmpty())
+            setMainPhotoPostImageView(model.getImage0());
+        else if (model.getImage1() != null && !model.getImage1().isEmpty())
+            setMainPhotoPostImageView(model.getImage1());
+        else if (model.getImage2() != null && !model.getImage2().isEmpty())
+            setMainPhotoPostImageView(model.getImage2());
+        else if (model.getImage3() != null && !model.getImage3().isEmpty())
+            setMainPhotoPostImageView(model.getImage3());
+        else if (model.getImage4() != null && !model.getImage4().isEmpty())
+            setMainPhotoPostImageView(model.getImage4());
+        else if (model.getImage5() != null && !model.getImage5().isEmpty())
+            setMainPhotoPostImageView(model.getImage5());
+        else if (model.getImage6() != null && !model.getImage6().isEmpty())
+            setMainPhotoPostImageView(model.getImage6());
+        else if (model.getImage7() != null && !model.getImage7().isEmpty())
+            setMainPhotoPostImageView(model.getImage7());
 
     }
 
@@ -60,12 +77,26 @@ public class PostsAdapter extends FirestoreRecyclerAdapter<Post, PostsAdapter.Vi
     class ViewHolder extends RecyclerView.ViewHolder {
         TextView titlePostTextView;
         TextView pricePostTextView;
+        TextView categoryPostTextView;
+        TextView locationPostTextView;
+        TextView bedroomPostTextView;
+        TextView bathroomPostTextView;
+        TextView sqmPostTextView;
+        TextView floorPostTextView;
+        TextView antiquityPostTextView;
         ImageView mainPhotoPostImageView;
 
         public ViewHolder(View view) {
             super(view);
             titlePostTextView = view.findViewById(R.id.titlePostTextView);
             pricePostTextView = view.findViewById(R.id.pricePostTextView);
+            categoryPostTextView = view.findViewById(R.id.categoryPostTextView);
+            locationPostTextView = view.findViewById(R.id.locationPostTextView);
+            bedroomPostTextView = view.findViewById(R.id.bedroomPostTextView);
+            bathroomPostTextView = view.findViewById(R.id.bathroomPostTextView);
+            sqmPostTextView = view.findViewById(R.id.sqmPostTextView);
+            floorPostTextView = view.findViewById(R.id.floorPostTextView);
+            antiquityPostTextView = view.findViewById(R.id.antiquityPostTextView);
             mainPhotoPostImageView = view.findViewById(R.id.mainPhotoPostImageView);
         }
     }
